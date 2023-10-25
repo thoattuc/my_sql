4d055ea2d88959799743f99fdea25ec4a615de37
thoattuc <thoattuc.hd@gmail.com>
2023-10-25 10:24:55 +0700

    Commit

--- script/niam_elearning.sql
+++ script/niam_elearning.sql
@@ -6,17 +6,16 @@ CREATE TABLE users(
 	id int not null auto_increment,
     name varchar(255) not null,
     email varchar(255) not null,
+	idRole int(11) not null,
+    phone char(10) null default null,
     status tinyint not null default 1,
     password char(80) not null,
     remember_token varchar(80) null default null,
-    idRole int(11) not null,
     create_at TIMESTAMP null default null,
     update_at TIMESTAMP null,
-    primary key (id)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+    primary key (id),
+    FOREIGN KEY (idRole) REFERENCES user_roles(id)
+);
 
 create table user_roles (
 	id int auto_increment,
@@ -25,10 +24,9 @@ create table user_roles (
     create_at TIMESTAMP null,
     update_at TIMESTAMP null,
     primary key(id)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+);
+
+-- --------------------------------------------------
 
 create table educations (
 	id int auto_increment,
@@ -39,68 +37,107 @@ create table educations (
     primary key(id)
 );
 
-create table categrories (
+create table course_edu (
 	id int auto_increment,
     name varchar(255),
+    idEdu int not null,
     status boolean default 1,
-    idEducation int not null,
     create_at TIMESTAMP null,
     update_at TIMESTAMP null,
     primary key(id)
 );
+
+alter TABLE course_edu
+add FOREIGN KEY (idEdu) REFERENCES educations(id);
 
 create table course (
 	id int auto_increment,
-    name varchar(255),
-    duration TIME,
+    name varchar(255) not null,
+	summary varchar(255) DEFAULT null,
     price int not null,
+    image char(200) DEFAULT null,
     discount int null,
-    summary varchar(255),
+	idCoEd int not null,
+    duration int not null,
+    grade VARCHAR(100),
 	status boolean default 1,
 	description text,
-    idCate int not null,
     create_at TIMESTAMP null,
     update_at TIMESTAMP null,
     primary key(id)
 );
 
+alter table course
+add FOREIGN KEY (idCoEd) REFERENCES course_edu(id);
+
+-- -------------------------------------------------
+
 create table schedules (
 	id int auto_increment,
+    schedule text,
+    idTeacher int not null,
     idCourse int not null,
-    time TIMESTAMP,
     idUser int not null,
 	create_at TIMESTAMP null,
     update_at TIMESTAMP null,
     primary key(id)
 );
 
+alter TABLE schedules
+add FOREIGN KEY (idCourse) REFERENCES course(id);
+
+-- --------------------------------------------
+
 create table bills (
 	id int auto_increment,
+    name varchar(255),
+    email CHAR(255),
+    phone CHAR(10),
     idSchedule int not null,
+    status BOOLEAN DEFAULT 1,
     create_at TIMESTAMP null,
     update_at TIMESTAMP null,
     primary key(id)
 );
 
+alter table bills
+add FOREIGN KEY (idSchedule) REFERENCES schedules(id);
+
 create table process (
 	id int auto_increment,
+    name VARCHAR(255),
     idTeacher int not null,
-    idSchedule int not null,
+	schedules CHAR(255),
+    idCourse int not null,
 	duration TIME,
+    pass int not null,
     primary key(id)
 );
 
-create table detail (
+alter table process
+add FOREIGN KEY (idCourse) REFERENCES course(id);
+
+create table process_user (
 	id int auto_increment,
-    id_student int not null,
+    idProcess int not null,
+    idUser int not null,
     primary key(id)
 );
+
+ALTER TABLE process_user
+add FOREIGN KEY (idProcess) REFERENCES process(id);
+
+ALTER TABLE process_user
+add FOREIGN KEY (idUser) REFERENCES users(id);
+
+-- --------------------------------------------------------
 
 use niam_elearning;
 ALTER TABLE users
 add FOREIGN KEY (idRole) REFERENCES user_roles(id);
 
-
+-- --------------------------------------------------------
+use niam_elearning;
 select * from niam_elearning.users_role;
 
 -- Hàm:
@@ -112,14 +149,38 @@ values ('customer', (current_timestamp()));
 -- Hàm:
 SELECT FLOOR(RAND() * (9999 - 1 + 1)) + 1;
 
-INSERT INTO users(name, email, idRole, password, created_at)
-VALUES('nam', 'name@gmail.com', 1, '12345',(current_timestamp()));
 
-INSERT INTO users(name, email, idRole, password, created_at)
-VALUES('nam', 'name@gmail.com', 1, '12345',(current_timestamp()));
 
-INSERT INTO users(name, email, idRole, password, created_at)
-VALUES('nam', 'name@gmail.com', 1, '12345',(current_timestamp()));
+insert into user_roles(name, create_at)
+values('admin', (current_timestamp()));
 
-INSERT INTO users(name, email, idRole, password, created_at)
-VALUES('nam', 'name@gmail.com', 1, '12345',(current_timestamp()));
+insert into user_roles(name, create_at)
+values('user', (current_timestamp()));
+
+insert into user_roles(name, create_at)
+values('customer', (current_timestamp()));
+
+ALTER TABLE user_roles
+ADD CONSTRAINT  UNIQUE (name);
+
+-- -------------------------------------------------------------
+
+INSERT INTO users(name, email, idRole, password, create_at)
+VALUES('nam', 'name@gmail.com', 2, '12345',(current_timestamp()));
+
+INSERT INTO users(name, email, idRole, password, create_at)
+VALUES('admin', 'admin@gmail.com', 1, '12345',(current_timestamp()));
+
+INSERT INTO users(name, email, idRole, password, create_at)
+VALUES('abc', 'abc@gmail.com', 2, '12345',(current_timestamp()));
+
+INSERT INTO users(name, email, idRole, password, create_at)
+VALUES('alien', 'alien@gmail.com', 3, '12345',(current_timestamp()));
+
+-- ---------------------------------------------------------------
+
+insert into educations(name, create_at)
+values
+	('Chương trình IELTS',(current_timestamp())),
+	('Chương trình phổ thông quốc tế Cambridge',(current_timestamp())),
+	('Chương trình lập trình',(current_timestamp()));
